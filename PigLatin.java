9b7ad1bdccd2c19e7d3c566480840dfbd4b1f16e
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-28 15:05:47 -0500

    added pigLatinSimple and tests

--- PigLatin.java
+++ PigLatin.java
@@ -1,9 +1,16 @@
 public class PigLatin{
-/*
-  public static String pigLatinSimple(String s){
 
+  public static String pigLatinSimple(String s){
+    String[] vowels = {"a", "e", "i", "o", "u"};
+    String vowels1 = "aeiou";
+    String char0 = s.substring(0,1);
+    if (vowels1.contains(char0))
+      return s + "hay";
+    else
+      return s.substring(1, s.length()) + char0 + "ay";
   }
 
+/*
   public static String pigLatin(String s){
 
   }
@@ -12,7 +19,16 @@ public class PigLatin{
 
   }
 */
-}
 
+
+  public static void main(String[] args){
+    System.out.println("PigLatinSimple tests:");
+    System.out.println(pigLatinSimple("mock")); //ockmay
+    System.out.println(pigLatinSimple("pie")); //iepay
+    System.out.println(pigLatinSimple("david")); //avidday
+    System.out.println(pigLatinSimple("aaron")); //aaronhay
+
+  }
+}
 // command: java PigLatin < book.txt
 // command: cat book.txt | java PigLatin
