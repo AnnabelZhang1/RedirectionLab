b936b34eeaeb4a2c5a1e9285d013e4654aae6c07
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-28 12:30:41 -0500

    cleared up comments and tested with two txt files

--- MakeStars.java
+++ MakeStars.java
@@ -1,21 +1,8 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.*;
 
 public class MakeStars{
 
-  public static void main(String[] args) throws FileNotFoundException{
-    System.out.println("ORIGINAL:");
-    System.out.println("-------------------------------------");
-//    File text = new File(System.in);
-    Scanner n = new Scanner(System.in);
-    int lineNum = 1;
-    while (n.hasNextLine()){
-      String line = n.nextLine();
-      System.out.println(line);
-      lineNum++;
-    }
+  public static void main(String[] args){
 
     System.out.println();
     System.out.println("STARRED:");
@@ -24,20 +11,17 @@ public class MakeStars{
     Scanner starred = new Scanner(System.in);
     while (starred.hasNextLine()){
       String line = starred.nextLine();
-
       Scanner linescan = new Scanner(line);
+
       while (linescan.hasNext()){
         String s = linescan.next();
-
         for (int i = 0; i < s.length(); i++)
           System.out.print("*");
         if (linescan.hasNext())
           System.out.print(" ");
       }
-
       if (starred.hasNextLine())
         System.out.println();
-
     }
   }
 }
