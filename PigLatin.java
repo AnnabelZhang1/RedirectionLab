ae8aa4ba7bf1adc2b173f8c93bc4cd35b7bea63a
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-28 12:31:38 -0500

    started PigLatin

---
+++ PigLatin.java
@@ -0,0 +1,18 @@
+public class PigLatin{
+/*
+  public static String pigLatinSimple(String s){
+
+  }
+
+  public static String pigLatin(String s){
+
+  }
+
+  public static String pigLatinBest(String s){
+
+  }
+*/
+}
+
+// command: java PigLatin < book.txt
+// command: cat book.txt | java PigLatin
