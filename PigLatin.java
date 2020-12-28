c46d80e7ede27b6e320b7f2361f0efa914b284a9
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-28 17:36:54 -0500

    added pigLatin method and tests

--- PigLatin.java
+++ PigLatin.java
@@ -1,25 +1,35 @@
 public class PigLatin{
 
+  private static String vowels = "aeiou";
+
   public static String pigLatinSimple(String s){
-    String[] vowels = {"a", "e", "i", "o", "u"};
-    String vowels1 = "aeiou";
     String char0 = s.substring(0,1);
-    if (vowels1.contains(char0))
+    if (vowels.contains(char0))
       return s + "hay";
     else
       return s.substring(1, s.length()) + char0 + "ay";
   }
 
-/*
   public static String pigLatin(String s){
+    String char0 = s.substring(0,1);
+    String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr",
+                          "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc",
+                          "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th",
+                          "tr", "tw", "wh", "wr"};
 
+    for (String element : diagraphs){
+      if (s.contains(element))
+        return s.substring(2, s.length()) + s.substring(0,2) + "ay";
+      else if (vowels.contains(s.substring(0,1)))
+        return s + "hay";
+    }
+    return s.substring(1, s.length()) + char0 + "ay";
   }
-
+/*
   public static String pigLatinBest(String s){
 
   }
 */
-
 
   public static void main(String[] args){
     System.out.println("PigLatinSimple tests:");
@@ -28,7 +38,16 @@ public class PigLatin{
     System.out.println(pigLatinSimple("david")); //avidday
     System.out.println(pigLatinSimple("aaron")); //aaronhay
 
+    System.out.println();
+
+    System.out.println("PigLatin tests:");
+    System.out.println(pigLatin("the")); //ethay
+    System.out.println(pigLatin("check")); //eckchay
+    System.out.println(pigLatin("skee")); //eeskay
+    System.out.println(pigLatin("emu")); //emuhay
+    System.out.println(pigLatin("grade")); //adegray
   }
 }
+
 // command: java PigLatin < book.txt
 // command: cat book.txt | java PigLatin
