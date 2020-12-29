066ba6d5648352d5d109f8347e778fbd97d0878e
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-28 22:23:06 -0500

    fixed pigLatin and added pigLatinBest

--- PigLatin.java
+++ PigLatin.java
@@ -3,14 +3,16 @@ public class PigLatin{
   private static String vowels = "aeiou";
 
   public static String pigLatinSimple(String s){
+    s = s.toLowerCase();
     String char0 = s.substring(0,1);
     if (vowels.contains(char0))
-      return (s + "hay").toLowerCase();
+      return s + "hay";
     else
-      return (s.substring(1, s.length()) + char0 + "ay").toLowerCase();
+      return s.substring(1, s.length()) + char0 + "ay";
   }
 
   public static String pigLatin(String s){
+    s = s.toLowerCase();
     String char0 = s.substring(0,1);
     String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr",
                           "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc",
@@ -18,18 +20,24 @@ public class PigLatin{
                           "tr", "tw", "wh", "wr"};
 
     for (String element : diagraphs){
-      if (s.contains(element))
-        return (s.substring(2, s.length()) + s.substring(0,2) + "ay").toLowerCase();
+      if (element.contains(s.substring(0,2)))
+        return s.substring(2, s.length()) + s.substring(0,2) + "ay";
       else if (vowels.contains(s.substring(0,1)))
-        return (s + "hay").toLowerCase();
+        return s + "hay";
     }
-    return (s.substring(1, s.length()) + char0 + "ay").toLowerCase();
+    return pigLatinSimple(s);
   }
-/*
-  public static String pigLatinBest(String s){
 
+  public static String pigLatinBest(String s){
+    s = s.toLowerCase();
+    if (!Character.isLetter(s.charAt(0)))
+      return s;
+    int lastIndex = s.length() - 1;
+    char lastElement = s.charAt(s.length() - 1);
+    if (!Character.isLetter(lastElement))
+      return pigLatin(s.substring(0, lastIndex)) + lastElement;
+    return pigLatin(s);
   }
-*/
 
   public static void main(String[] args){
     System.out.println("PigLatinSimple tests:");
@@ -40,12 +48,24 @@ public class PigLatin{
 
     System.out.println();
 
-    System.out.println("PigLatin tests:");
+    System.out.println("pigLatin tests:");
     System.out.println(pigLatin("tHe")); //ethay
     System.out.println(pigLatin("check")); //eckchay
     System.out.println(pigLatin("skee")); //eeskay
     System.out.println(pigLatin("emu")); //emuhay
     System.out.println(pigLatin("grade")); //adegray
+
+    System.out.println();
+
+    System.out.println("pigLatinBest tests:");
+    System.out.println(pigLatinBest("*emu")); //*emu
+    System.out.println(pigLatinBest("4chan")); //4chan
+    System.out.println(pigLatinBest("fish!")); //ishfay! \\
+    System.out.println(pigLatinBest("fish")); //ishfay \\
+    System.out.println(pigLatinBest("the.")); //ethay.
+    System.out.println(pigLatinBest("cat!")); //atcay!
+    System.out.println(pigLatinBest("amazing?")); //amazinghay?
+    System.out.println(pigLatinBest("apple%")); //applehay%
   }
 }
 
